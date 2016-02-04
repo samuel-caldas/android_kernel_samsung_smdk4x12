6bff6df730716bc7e3900c2a1164a176e40f2da2
Dave Martin <davemartinfsu@gmail.com>
2016-02-03 21:01:23 -0500

    set default 1.8ghz and voltage

--- arch/arm/mach-exynos/cpufreq.c
+++ arch/arm/mach-exynos/cpufreq.c
@@ -747,7 +747,7 @@ static int exynos_cpufreq_cpu_init(struct cpufreq_policy *policy)
 	retval = cpufreq_frequency_table_cpuinfo(policy, exynos_info->freq_table);
 
 	/* Keep stock frq. as default startup frq. */
-	policy->max = 1600000;
+	policy->max = 1800000;
 	policy->min = 200000;
 
 	return retval;
